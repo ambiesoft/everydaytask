3993b18ff85751bd1ea9a206fd4cf1ce3af0ff97
ambiesoft <ambiesoft.trueff@gmail.com>
2023-04-01 17:46:47 +0900

    able to create a new spread by client script

--- src/func.js
+++ src/func.js
@@ -15,8 +15,11 @@ const hours = today.getHours().toString().padStart(2, '0');
 const minutes = today.getMinutes().toString().padStart(2, '0');
 const seconds = today.getSeconds().toString().padStart(2, '0');
 
+const SPREAD_NAME = "EverydayTask.Ambiesoft.com_16053688-8F91-400D-8CBE-4AF19E561586";
 const UNCHECKMARK = "☐";
 const CHECKMARK = "☑️";
+
+const COOKIE_SHEETID = "SheetID";
 
 function gisLoaded() {
   console.log("gisLoaded is called")
@@ -73,6 +76,7 @@ window.onload = () => {
   // }
   console.log(gapi);
   toggleLoginButton(gapi.client.getToken() != null);
+  console.log("sheet id", Cookies.get(COOKIE_SHEETID));
 }
 
 function ensureToken() {
@@ -243,24 +247,361 @@ function getFile() {
     console.log(data);
   })
 }
-function createNewGoogleSheet() {
-  var fileMetadata = {
-    'name': 'newSheet',
-    'parents': ['appDataFolder']
+
+// https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/create
+async function doCreateNewGoogleSheet() {
+  var spreadsheetBody = {
+    properties: {
+      "title": `${SPREAD_NAME}`,
+    },
+    "sheets": [
+      // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/sheets#Sheet
+      {
+        // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/sheets#SheetProperties
+        "properties": {
+          "title": "README",
+          "index": 0,
+          "sheetType": "GRID",
+          "gridProperties": {
+            "rowCount": 1000,
+            "columnCount": 26
+          }
+        },
+        "data": [
+          {
+            // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/sheets#GridData
+            "startRow": 0,
+            "startColumn": 0,
+            "rowData": [
+              {
+                // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/sheets#RowData
+                "values": [
+                  {
+                    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/cells#CellData
+                    "userEnteredValue": {
+                      // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/other#ExtendedValue
+                      "stringValue": "AAAAAAAAA",
+                    },
+                  },
+                ],
+              },
+              {
+                // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/sheets#RowData
+                "values": [
+                  {
+                    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/cells#CellData
+                    "userEnteredValue": {
+                      // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/other#ExtendedValue
+                      "stringValue": "BBBBBBBBBBBBBBBB",
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        "properties": {
+          "title": "Tasks",
+          "index": 1,
+          "sheetType": "GRID",
+          "gridProperties": {
+            "rowCount": 1000,
+            "columnCount": 26
+          }
+        }
+      }
+    ],
   };
-  var media = {
-    mimeType: 'application/vnd.google-apps.spreadsheet',
-    body: ''
-  };
-  const request = gapi.client.drive.files.create({
-    resource: fileMetadata,
-    media,
-    fields: 'id'
-  })
-  request.execute((data) => {
-    console.log(data);
-  })
+
+  const res = await gapi.client.sheets.spreadsheets.create({}, spreadsheetBody);
+
+  /*
+  {
+      "result": {
+          "spreadsheetId": "1crN_IU_k2m-oEDiPIIrjKpeshVCrV7J7d6pe5DLBHDY",
+          "properties": {
+              "title": "EverydayTask.Ambiesoft.com_16053688-8F91-400D-8CBE-4AF19E561586",
+              "locale": "ja_JP",
+              "autoRecalc": "ON_CHANGE",
+              "timeZone": "Etc/GMT",
+              "defaultFormat": {
+                  "backgroundColor": {
+                      "red": 1,
+                      "green": 1,
+                      "blue": 1
+                  },
+                  "padding": {
+                      "top": 2,
+                      "right": 3,
+                      "bottom": 2,
+                      "left": 3
+                  },
+                  "verticalAlignment": "BOTTOM",
+                  "wrapStrategy": "OVERFLOW_CELL",
+                  "textFormat": {
+                      "foregroundColor": {},
+                      "fontFamily": "arial,sans,sans-serif",
+                      "fontSize": 10,
+                      "bold": false,
+                      "italic": false,
+                      "strikethrough": false,
+                      "underline": false,
+                      "foregroundColorStyle": {
+                          "rgbColor": {}
+                      }
+                  },
+                  "backgroundColorStyle": {
+                      "rgbColor": {
+                          "red": 1,
+                          "green": 1,
+                          "blue": 1
+                      }
+                  }
+              },
+              "spreadsheetTheme": {
+                  "primaryFontFamily": "Arial",
+                  "themeColors": [
+                      {
+                          "colorType": "TEXT",
+                          "color": {
+                              "rgbColor": {}
+                          }
+                      },
+                      {
+                          "colorType": "BACKGROUND",
+                          "color": {
+                              "rgbColor": {
+                                  "red": 1,
+                                  "green": 1,
+                                  "blue": 1
+                              }
+                          }
+                      },
+                      {
+                          "colorType": "ACCENT1",
+                          "color": {
+                              "rgbColor": {
+                                  "red": 0.25882354,
+                                  "green": 0.52156866,
+                                  "blue": 0.95686275
+                              }
+                          }
+                      },
+                      {
+                          "colorType": "ACCENT2",
+                          "color": {
+                              "rgbColor": {
+                                  "red": 0.91764706,
+                                  "green": 0.2627451,
+                                  "blue": 0.20784314
+                              }
+                          }
+                      },
+                      {
+                          "colorType": "ACCENT3",
+                          "color": {
+                              "rgbColor": {
+                                  "red": 0.9843137,
+                                  "green": 0.7372549,
+                                  "blue": 0.015686275
+                              }
+                          }
+                      },
+                      {
+                          "colorType": "ACCENT4",
+                          "color": {
+                              "rgbColor": {
+                                  "red": 0.20392157,
+                                  "green": 0.65882355,
+                                  "blue": 0.3254902
+                              }
+                          }
+                      },
+                      {
+                          "colorType": "ACCENT5",
+                          "color": {
+                              "rgbColor": {
+                                  "red": 1,
+                                  "green": 0.42745098,
+                                  "blue": 0.003921569
+                              }
+                          }
+                      },
+                      {
+                          "colorType": "ACCENT6",
+                          "color": {
+                              "rgbColor": {
+                                  "red": 0.27450982,
+                                  "green": 0.7411765,
+                                  "blue": 0.7764706
+                              }
+                          }
+                      },
+                      {
+                          "colorType": "LINK",
+                          "color": {
+                              "rgbColor": {
+                                  "red": 0.06666667,
+                                  "green": 0.33333334,
+                                  "blue": 0.8
+                              }
+                          }
+                      }
+                  ]
+              }
+          },
+          "sheets": [
+              {
+                  "properties": {
+                      "sheetId": 0,
+                      "title": "シート1",
+                      "index": 0,
+                      "sheetType": "GRID",
+                      "gridProperties": {
+                          "rowCount": 1000,
+                          "columnCount": 26
+                      }
+                  }
+              }
+          ],
+          "spreadsheetUrl": "https://docs.google.com/spreadsheets/d/1crN_IU_k2m-oEDiPIIrjKpeshVCrV7J7d6pe5DLBHDY/edit"
+      },
+      "status": 200,
+      "statusText": null
+  }
+  */
+  console.log(res);
+  return res.result.spreadsheetId;
 }
+async function doCreateNewGoogleSheet_error() {
+  let res = await gapi.client.drive.files.create({
+    resource: {
+      name: 'aaaaa',
+      parents: ['appDataFolder'],
+      mimeType: 'application/vnd.google-apps.spreadsheet'
+    },
+    fields: 'id'
+  });
+  /*
+  {
+      "result": {
+          "error": {
+              "code": 403,
+              "message": "Method not supported for files within the Application Data folder.",
+              "errors": [
+                  {
+                      "message": "Method not supported for files within the Application Data folder.",
+                      "domain": "global",
+                      "reason": "notSupportedForAppDataFolderFiles"
+                  }
+              ]
+          }
+      },
+      "body": "{\n  \"error\": {\n    \"code\": 403,\n    \"message\": \"Method not supported for files within the Application Data folder.\",\n    \"errors\": [\n      {\n        \"message\": \"Method not supported for files within the Application Data folder.\",\n        \"domain\": \"global\",\n        \"reason\": \"notSupportedForAppDataFolderFiles\"\n      }\n    ]\n  }\n}\n",
+      "headers": {
+          "cache-control": "private, max-age=0",
+          "content-encoding": "gzip",
+          "content-length": "180",
+          "content-type": "application/json; charset=UTF-8",
+          "date": "Sat, 01 Apr 2023 07:43:23 GMT",
+          "expires": "Sat, 01 Apr 2023 07:43:23 GMT",
+          "server": "ESF",
+          "vary": "Origin, X-Origin"
+      },
+      "status": 403,
+      "statusText": null
+  }
+  */
+  console.log(res);
+  return res;
+}
+
+function OnGetTasksGoogleSheet() {
+  getTasksGoogleSheet();
+}
+async function getTasksGoogleSheet() {
+  let sheetid = Cookies.get(COOKIE_SHEETID);
+  if (sheetid) {
+    console.log("sheet id is in cookie", sheetid);
+    return;
+  }
+  try {
+    sheetid = await doGetTasksGoogleSheet();
+    if (!sheetid) {
+      sheetid = await doCreateNewGoogleSheet();
+    }
+    if (!sheetid) {
+      throw new Error("Failed to get Google Sheet");
+    }
+    Cookies.set(COOKIE_SHEETID, sheetid); // { expires: 30 });
+    console.log("sheet id", Cookies.get(COOKIE_SHEETID));
+  } catch (e) {
+    console.error(e);
+  }
+}
+// https://developers.google.com/drive/api/v3/reference/files/list
+async function doGetTasksGoogleSheet() {
+  let res = await gapi.client.drive.files.list({
+    q: `mimeType='application/vnd.google-apps.spreadsheet' and name='${SPREAD_NAME}'`,
+    // fields: 'nextPageToken, files(id, name)',
+    // spaces: 'appDataFolder',
+  });
+  /*
+{
+    "result": {
+        "kind": "drive#fileList",
+        "incompleteSearch": false,
+        "files": [
+            {
+                "kind": "drive#file",
+                "mimeType": "application/vnd.google-apps.spreadsheet",
+                "id": "1vA8F9N2sn4BhcbQshsqneBV_WdWsAZ50_q57jtEgkvU",
+                "name": "EverydayTask.Ambiesoft.com_16053688-8F91-400D-8CBE-4AF19E561586"
+            }
+        ]
+    },
+    "body": "{\n  \"kind\": \"drive#fileList\",\n  \"incompleteSearch\": false,\n  \"files\": [\n    {\n      \"kind\": \"drive#file\",\n      \"mimeType\": \"application/vnd.google-apps.spreadsheet\",\n      \"id\": \"1vA8F9N2sn4BhcbQshsqneBV_WdWsAZ50_q57jtEgkvU\",\n      \"name\": \"EverydayTask.Ambiesoft.com_16053688-8F91-400D-8CBE-4AF19E561586\"\n    }\n  ]\n}\n",
+    "headers": {
+        "cache-control": "no-cache, no-store, max-age=0, must-revalidate",
+        "content-encoding": "gzip",
+        "content-length": "251",
+        "content-type": "application/json; charset=UTF-8",
+        "date": "Sat, 01 Apr 2023 07:01:18 GMT",
+        "expires": "Mon, 01 Jan 1990 00:00:00 GMT",
+        "pragma": "no-cache",
+        "server": "ESF",
+        "vary": "Origin, X-Origin"
+    },
+    "status": 200,
+    "statusText": null
+}  
+  */
+  console.log(res);
+  if (res.result.files.length != 0) {
+    return res.result.files[0].id;
+  }
+  return null;
+}
+
+
+
+// gapi.client.sheets.spreadsheets.create({
+//   "properties": {
+//     "title": "mysheet",
+//   },
+//   "sheets": [
+//     "sheet1",
+//     "sheet2",
+//     "sheet3",
+//   ]
+// }).then(function (response) {
+//   console.log(response);
+// });
+
+// }
+
 function updateValues(spreadsheetId, range, valueInputOption, _values, callback) {
   let values = [
     [
